bf7ad99a9b1a90f3437ef5bab74fe4cf0c7b3065
eatingwounds <ta2053313@gmail.com>
2025-01-30 23:11:12 +0500

    making a copy
    если что потворяю все что с видео, до этогшо были попытки понять что значит input  с помощью чата, понял но свой пример вывести не смог.

---
+++ jajanken.py
@@ -0,0 +1,10 @@
+import random 
+def play():
+    user = input("'r' for rock 'p' for paper 's' for scissors")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'tie' 
+    #r > s, s > p, p > r
+def is_win(player, opponent):
+    # return true if player wins
